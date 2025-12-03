0594f30a3599fbf55d14447eb648e7b594ca1557
redmi <hyq@starj.com.cn>
2025-12-03 17:10:35 +0800

    'gd_map'

--- pages/GdMap/GdMap.js
+++ pages/GdMap/GdMap.js
@@ -8,6 +8,26 @@ Page({
     markers: [],
     address: ''
   },
+  onMapTap(e) {
+    // 官方已换算好
+    const { longitude, latitude } = e.detail
+    console.log('点击处坐标:', longitude, latitude)
+    // 如果想把箭头移到该点
+    this.setData({
+      'markers[0].longitude': longitude,
+      'markers[0].latitude': latitude
+    })
+  },
+  onRegionChange(e) {
+    if (e.type === 'end' || e.type === 'drag') {   // 只要拖动中/拖动结束
+      this.mapCtx = this.mapCtx || wx.createMapContext('gaodeMap')
+      this.mapCtx.getCenterLocation({            // 官方接口，毫秒级
+        success: res => {
+          console.log('中心点经纬度:', res.longitude, res.latitude)
+        }
+      })
+    }
+  },
   onLoad() {
     // 1. 实例化高德接口
     this.amapPlugin = new amapFile.AMapWX({ key: '0a0ac2a2c7f1615b99070b18ddded0c2' })
@@ -16,11 +36,26 @@ Page({
   },
   /* 微信定位 */
   getLocation() {
-    wx.getLocation({ type: 'gcj02', success: res => {
-        this.setData({ lat: res.latitude, lng: res.longitude })
-        this.reverseGeo(res.latitude, res.longitude)
-      }
+    // const { lat, lng } = this.data;
+    var m_lat = 39.90923
+    var m_lon = 116.39747
+    this.setData({ lat:m_lat, lng: m_lon})
+    this.reverseGeo(m_lat, m_lon)
+    this.setData({
+      lat: m_lat,
+      lng: m_lon,
+      markers: [{
+        id: 1,
+        latitude: m_lat,   // ← 原来是 m_lat
+        longitude: m_lon,  // ← 原来是 m_lon
+        iconPath: '../../resource/north.png',
+        width: 30,
+        height: 30,
+        rotate: 80,         // 如果想让箭头指北就写 0
+        anchor: { x: 0.5, y: 0.5 }
+      }]
     })
+
   },
   /* 逆地理 */
   reverseGeo(lat, lng) {
@@ -34,21 +69,21 @@ Page({
   },
   /* 周边搜索 */
   getPOI() {
-    this.amapPlugin.getPoiAround({
-      location: `${this.data.lng},${this.data.lat}`,
-      querytypes: '010000',   // 加油站
-      radius: 500,
-      success: data => {
-        const mks = data.pois.map(p => ({
-          id: p.id,
-          latitude: p.location.lat,
-          longitude: p.location.lng,
-          iconPath: '/img/marker.png',  // 32×32
-          width: 32, height: 32,
-          callout: { content: p.name, display: 'ALWAYS' }
-        }))
-        this.setData({ markers: mks })
-      }
-    })
+    // this.amapPlugin.getPoiAround({
+    //   location: `${this.data.lng},${this.data.lat}`,
+    //   querytypes: '010000',   // 加油站
+    //   radius: 500,
+    //   success: data => {
+    //     const mks = data.pois.map(p => ({
+    //       id: p.id,
+    //       latitude: p.location.lat,
+    //       longitude: p.location.lng,
+    //       iconPath: '/img/marker.png',  // 32×32
+    //       width: 32, height: 32,
+    //       callout: { content: p.name, display: 'ALWAYS' }
+    //     }))
+    //     this.setData({ markers: mks })
+    //   }
+    // })
   }
 })
